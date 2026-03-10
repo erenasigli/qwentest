bf8ceae73c50e0b26de1102f75d48d891f15483b
servissoftmac6 <servissoftmac6@macmini6>
2026-03-10 17:04:51 +0300

    AI: Added a Footer component that displays '2026' at the bottom center of the page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
     <div className='flex flex-col justify-center items-center h-screen bg-gray-100'>
       <HelloWorld />
       <a href='https://www.google.com' target='_blank' rel='noopener noreferrer' className='mt-4 text-blue-500 underline'>Go to Google</a>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className='absolute bottom-0 w-full text-center p-4 bg-gray-800 text-white'>
+      2026
+    </footer>
+  );
+};
+
+export default Footer;
