e765f7ecc435fd198767e459cf8811f35c5740ce
servissoftmac6 <servissoftmac6@macmini6>
2026-03-11 01:38:47 +0300

    AI: Removed the Google link from App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ const App: React.FC = () => {
   return (
     <div className='flex flex-col justify-center items-center h-screen bg-gray-100'>
       <HelloWorld />
-      <a href='https://www.google.com' target='_blank' rel='noopener noreferrer' className='mt-4 text-blue-500 underline'>Go to Google</a>
       <Footer />
     </div>
   );
